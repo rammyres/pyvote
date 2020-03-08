f39b1aca2f7d3eaa1ae9c5dfe21f73b4b7452ab3
rammyres <rammyres@ufpi.edu.br>
2020-03-07 21:50:15 -0300

    Versão 0.2.2, incluidas algumas melhorias e codigo todo comentado

--- block.py
+++ block.py
@@ -21,9 +21,13 @@ class Block:
     meu_hash = None #Hash do bloco atual
 
     def __init__(self, modo, tipo, index, dados, hash_anterior, numero = None, aleatorio = None, nonce = None, meuhash = None):
-        #print("{} {} {} {} {} {} {} {} {}".format(modo, tipo, index, dados, hash_anterior, numero, aleatorio, nonce, meuhash))
-        if modo == 1:
+        # O construtor dessa classe é sobrecarregado, permitindo instanciar em modo de inclusão (modo == 0) ou modo de importação
+        # (modo == 1). No caso do instanciamento em modo de importação, todos os dados devem ser recebidos e a classe faz poucas
+        # validações (basicamente dos tipo de bloco em importação). Uma das sobrecargas permite que omita-se o numero, para a 
+        # inclusão de blocos de votos
+        if modo == 1: # Instanciamento em modo de importação
             if tipo == "Genesis" or tipo == "candidato" or tipo == "voto":
+                # Caso o bloco tenha um tipo valido ele é importado integralmente 
                 self.index = index
                 self.tipoBloco = tipo
                 self.dados = dados 
@@ -34,28 +38,36 @@ class Block:
                 self.meu_hash = meuhash
 
             else:
+                # Caso não tenha um tipo valido de bloco levanta um erro 
                 raise tipoDeBlocoInvalido
         
         elif modo == 0:
+            # No modo de inclusão de bloco, dados gerados aleatoriamente (aleatorio e nonce)
+            # e a assinatura (meu_hash) são gerados pelo método crieMeuHash().
+
             if aleatorio != None or nonce != None or meuhash != None:
-                raise modoDeInclusaoInvalido
-            if tipo == "Genesis" or tipo == "candidato" or tipo == "voto":
-                self.index = index
+                raise modoDeInclusaoInvalido # Caso haja instanciamento em metodo de inclusão, com
+                                             # parametros aleatórios passados ao construtor o erro
+                                             # modoDeInclusaoInvalido é levantado
+            if tipo == "Genesis" or tipo == "candidato" or tipo == "voto": # Valida o tipo de bloco gerado
+                self.index = index 
                 self.tipoBloco = tipo
                 self.dados = dados
                 
                 if tipo == "voto":
-                    if tipo == "voto" and numero != None:
+                    if tipo == "voto" and numero != None: # Valida se o construtor recebeu um numero, caso o bloco 
+                                                          # seja um voto
                         raise vatoNaoPodeConterNumero
                     else:
                         self.numero = "0"
 
                 else:
-                    self.numero = numero
+                    self.numero = numero # Para inclusão e candidato, o numero é recebido no construtor
                 
                 self.hash_ant = hash_anterior
-                self.aleatorio = codecs.encode(os.urandom(32), 'hex').decode()
-                self.crieMeuHash()
+                self.aleatorio = codecs.encode(os.urandom(32), 'hex').decode() # Gera um hexadecimal aleatório, aumentando a dificuldade
+                                                                               # para quebra da assinatura dos blocos
+                self.crieMeuHash() # Gera o número único (nonce) e a assinatura (hash) do bloco
 
             else:
                 raise tipoDeBlocoInvalido
@@ -63,7 +75,7 @@ class Block:
 
         #No constutor da classe somente os tipos candidato, voto e genesis são aceitos
 
-        if tipo == "Genesis" and dados!="Bloco genesis":
+        if tipo == "Genesis" and dados!="Bloco genesis": # Verifica se o bloco Genesis foi bem formado 
             raise erroGenericoGenesis
 
 
--- erros.py
+++ erros.py
@@ -9,27 +9,32 @@ class Erros(Exception):
 
 class erroGenericoGenesis(Erros):
     # Caso a criação do bloco gênesis receba um número maior que 0 como parâmetro
+    
     def __init__(self):
         self.message = "O bloco genesis deve conter dados \'Bloco genesis\' e demais variáveis iguais a 0"
 
 class tipoDeBlocoInvalido(Erros):
-    
-    #Especifica o erro no caso da inclusão de um tipo de bloco inválido na bloco
+    # Especifica o erro no caso da inclusão de um tipo de bloco inválido na bloco
+
     def __init__(self):
         self.message = "O tipo de bloco deve ser \'Genesis\', \'voto\' ou \'candidato\'"
 
 class hashAnteriorInvalido(Erros):
-    #Erro de importação, no caso de conflito de bloco, no caso dele não conter
-    #um hash válido referente ao último bloco carregado
+    # Erro de importação, no caso de conflito de bloco, no caso dele não conter
+    # um hash válido referente ao último bloco carregado
+    
     def __init__(self):
         self.message = "O bloco atual não contém um hash válido para o bloco anterior"
 
 class vatoNaoPodeConterNumero(Erros):
+    # Erro de importação de blocos de voto, caso os mesmos contenham os dados no 
+    # campo número, ao invés do campo dados. 
 
     def __init__(self):
         self.message = "O voto não pode contar número"
 
 class modoDeInclusaoInvalido(Erros):
+    # Caso haja instanciamento em modo inclusão
 
     def __init__(self):
         self.message = "No instanciamento de novos blocos não pode haver a inclusão de nonce, hash ou aleatorio"
